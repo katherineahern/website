39d74f320d8f463ea5e9ee7647a35767caf0f540
katherineahern <kate@emergentdatasystems.com>
2019-12-29 11:15:19 -0800

    Change state from TextArea

--- src/views/HYA/HYA.jsx
+++ src/views/HYA/HYA.jsx
@@ -13,54 +13,63 @@ import avatar from "assets/img/faces/tigers_nest.jpg";
 
 
 
-function HYA({ ...props }) {
-  function handleChange(value) {
-    console.log("testId": value)
+class HYA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentAmount: 0,
+      amounts: [
+          {shortname: "id", amount: 8}, //– Legal ID for one participant
+          {shortname: "socks", amount: 35}, // – 25 pairs of socks
+          {shortname: "supplies", amount: 100}, // – One week of supplies for every group HYA offers
+          {shortname: "underwear", amount: 250}, // – 60 pairs of clean underwear
+          {shortname: "sleeping", amount: 500}, // – 33 sleeping bags
+          {shortname: "medicine", amount: 500},// – One month of medical supplies
+          {shortname: "food", amount: 5000}, // – Four months of healthy, nutritious food
+          {shortname: "home", amount: 5000000}, //– A place for us to call HOME
+        ]
+    };
+    this.handleChange = (event) => {
+      this.setState({currentAmount: event.target.value});
+    }
+    
   }
-  const profileStyle = {
-    width: '80%'
+  
+  
+  
+  render() {
+    console.log(this.state.currentAmount);
+    return (
+      <div>
+        <Grid container>
+          <ItemGrid xs={12} sm={12} md={12}  >
+            <Grid container spacing={12}>
+              <TextField
+                id="testId"
+                label="How much do you want to donate?"
+                defaultValue={this.state.currentAmount}
+                onChange={this.handleChange}
+                helperText="Some important text"
+              />
+            </Grid>
+            <Grid container spacing={3}>
+              
+               {this.state.amounts.map((value, index) => {
+                  return (
+                    <Grid item id={value.shortname} xs>
+                      <Paper>
+                        {value.shortname}
+                      </Paper>
+                    </Grid>
+                  );
+                })}
+                <Grid item xs>testId</Grid>
+            </Grid>
+          </ItemGrid>
+        </Grid>
+      </div>
+    );
   }
-  const amounts = [
-    {shortname: "id", amount: 8}, //– Legal ID for one participant
-    {shortname: "socks", amount: 35}, // – 25 pairs of socks
-    {shortname: "supplies", amount: 100}, // – One week of supplies for every group HYA offers
-    {shortname: "underwear", amount: 250}, // – 60 pairs of clean underwear
-    {shortname: "sleeping", amount: 500}, // – 33 sleeping bags
-    {shortname: "medicine", amount: 500},// – One month of medical supplies
-    {shortname: "food", amount: 5000}, // – Four months of healthy, nutritious food
-    {shortname: "home", amount: 5000000}, //– A place for us to call HOME
-
-  ]
-  return (
-    <div>
-      <Grid container>
-        <ItemGrid xs={12} sm={12} md={12} style={ profileStyle } >
-          <Grid container spacing={12}>
-            <TextField
-              id="testId"
-              label="How much do you want to donate?"
-              defaultValue="Default Value"
-              onChange={() => handleChange()}
-              helperText="Some important text"
-            />
-          </Grid>
-          <Grid container spacing={3}>
-            
-             {amounts.map((value, index) => {
-                return (
-                  <Grid item id={value.shortname} xs>
-                    <Paper>
-                      {value.shortname}
-                    </Paper>
-                  </Grid>
-                );
-              })}
-              <Grid item xs>testId</Grid>
-          </Grid>
-        </ItemGrid>
-      </Grid>
-    </div>
-  );
 }
 
 export default HYA;
